f787f9c9ec66b38314043f4b6915310660b439d5
Yang Hongbo <yanghongbo@iotpi.io>
2020-02-03 13:42:20 +0800

    added grip-mode

--- init.el
+++ init.el
@@ -99,7 +99,7 @@
     ("#dc322f" "#cb4b16" "#b58900" "#546E00" "#B4C342" "#00629D" "#2aa198" "#d33682" "#6c71c4")))
  '(package-selected-packages
    (quote
-    (lispy rust-mode vue-mode octave-mode tide tidal julia-repl color-theme-sanityinc-tomorrow zenburn-theme tango-2-theme dracula-theme julia-mode web-mode jinja2-mode flycheck elpy realgud php-mode git scss-mode django-snippets django-mode sass-mode json-mode typescript-mode docker-compose-mode dockerfile-mode yaml-mode ensime ecb magit cargo company racer slime)))
+    (grip-mode lispy rust-mode vue-mode octave-mode tide tidal julia-repl color-theme-sanityinc-tomorrow zenburn-theme tango-2-theme dracula-theme julia-mode web-mode jinja2-mode flycheck elpy realgud php-mode git scss-mode django-snippets django-mode sass-mode json-mode typescript-mode docker-compose-mode dockerfile-mode yaml-mode ensime ecb magit cargo company racer slime)))
  '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
  '(pos-tip-background-color "#073642")
  '(pos-tip-foreground-color "#93a1a1")
@@ -192,6 +192,11 @@
   :hook
   (lisp-mode . prettify-symbols-mode)
   (lisp-mode . company-mode))
+
+;; need to install grip
+(use-package grip-mode
+  :ensure t
+  :mode ("\\.md\\'"))
 
 ;; (add-to-list 'auto-mode-alist '("\\.jl\\'" . julia-mode))
 ;; (require 'julia-repl)
