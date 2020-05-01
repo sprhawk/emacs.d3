3ab2e087dc11a6a90f93cb1371da36229b482472
sprhawk <465558+sprhawk@users.noreply.github.com>
2020-05-01 20:20:13 +0800

    added ccls

--- init.el
+++ init.el
@@ -101,7 +101,7 @@
     ("#dc322f" "#cb4b16" "#b58900" "#546E00" "#B4C342" "#00629D" "#2aa198" "#d33682" "#6c71c4")))
  '(package-selected-packages
    (quote
-    (realgud-lldb lsp-mode rustic grip-mode afternoon-theme lispy rust-mode vue-mode octave-mode tide tidal julia-repl color-theme-sanityinc-tomorrow zenburn-theme tango-2-theme dracula-theme julia-mode web-mode jinja2-mode flycheck elpy realgud php-mode git scss-mode django-snippets django-mode sass-mode json-mode typescript-mode docker-compose-mode dockerfile-mode yaml-mode ensime ecb magit company racer slime)))
+    (ccls lsp-ui company-lsp realgud-lldb lsp-mode rustic grip-mode afternoon-theme lispy rust-mode vue-mode octave-mode tide tidal julia-repl color-theme-sanityinc-tomorrow zenburn-theme tango-2-theme dracula-theme julia-mode web-mode jinja2-mode flycheck elpy realgud php-mode git scss-mode django-snippets django-mode sass-mode json-mode typescript-mode docker-compose-mode dockerfile-mode yaml-mode ensime ecb magit company racer slime)))
  '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
  '(pos-tip-background-color "#073642")
  '(pos-tip-foreground-color "#93a1a1")
@@ -140,8 +140,22 @@
 ;;   ;;  )
 ;;   :mode ("\\.rs\\'" ))
 
-(use-package lsp-mode
+(use-package flycheck
   :ensure t)
+
+(use-package lsp-mode
+  :ensure t
+  :commands lsp)
+(use-package company-lsp
+  :ensure t
+  :commands company-lsp)
+(use-package lsp-ui
+  :ensure t
+  :commands lsp-ui-mode)
+(use-package ccls
+  :ensure t
+  :hook ((c-mode c++-mode objc-mode cuda-mode) .
+         (lambda () (require 'ccls) (lsp))))
 
 (use-package rustic
   :ensure t
