67f4f69c79946da4b3c52ef517124cc68ebdbce0
sprhawk <465558+sprhawk@users.noreply.github.com>
2021-07-01 11:46:26 +0800

    added (commented) original melpa url

--- packages.el
+++ packages.el
@@ -20,7 +20,10 @@
  package-archives '(("gnu" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
                     ("org" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/org/")
                     ("melpa" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
-                    ("melpa-stable" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/melpa-stable/")))
+                    ("melpa-stable" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/melpa-stable/")
+                    ;; ("melpa" . "https://melpa.org/packages/")
+                    ;; ("melpa-stable" . "https://stable.melpa.org/packages/")
+                    ))
 
 (package-initialize)
 (unless (package-installed-p 'use-package)
